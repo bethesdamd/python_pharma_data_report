3ba1b3fffd3c2bd4305d873ddce0259ce7e5b3d7
David Swearingen <swearingendw@mac.com>
2015-11-10 05:54:53 -0500

    needs more testing but this seems to be correctly processing multiple in files into multiple out files

--- drug_updates_format.py
+++ drug_updates_format.py
@@ -10,7 +10,6 @@ from os.path import isfile, join
 import sys
 
 cols = ['CompanyName','SpecificDrugProductName','SpecificDrugProductID', 'OnMarketDate','BrandGenericStatus','PackageSize','MarketClasses','WACBeginDate','WACPrice','WACUnitPrice']
-
 inpath = '/Users/david/Dropbox/send-to-david/'
 outpath = '/Users/david/Dropbox/FROM-david/'
 
@@ -18,8 +17,6 @@ today = datetime.date.today()
 day_s = str(today.day)
 day_s = '0' + day_s if len(day_s) == 1 else day_s 
 today_yyyymmdd = str(today.year) + "-" + str(today.month) + "-" + day_s
-
-print("Today is: " + today_yyyymmdd)
 
 onlyfiles = [ f for f in os.listdir(inpath) if isfile(join(inpath,f)) ]
 
@@ -33,20 +30,22 @@ if len(csv) == 0:
 print("The following files were received: \n" + '\n'.join(csv) + "\n")
 spacer = "\n-------------\n"
 
-fout = io.open(outpath + "report-" + today_yyyymmdd + ".txt", 'w', newline='\r\n')
 for filename in csv:
 	df = pd.read_csv(inpath + filename)
-
 	today = df[df.OnMarketDate.str.contains(today_yyyymmdd)]
-	for index, row in today.iterrows():
-		for col in cols:
-			text = col + ": " + str(row[col]) 
-			print(text)
-			fout.write(text + "\n")
-		print(spacer)
-		fout.write(spacer)
-	os.rename(inpath + filename, inpath + filename + ".DONE")
-fout.close()
+	if len(today.index) > 0:
+		fout = io.open(outpath + "report-" + today_yyyymmdd + "-from-file-" + filename + ".txt", 'w', newline='\r\n')
+		for index, row in today.iterrows():
+			print("inside index %s" % index)
+			for col in cols:
+				text = col + ": " + str(row[col]) 
+				print(text)
+				fout.write(text + "\n")
+			print(spacer)
+			fout.write(spacer)
+		fout.close()
+		os.rename(inpath + filename, inpath + filename + ".DONE")
+
 
 
 
